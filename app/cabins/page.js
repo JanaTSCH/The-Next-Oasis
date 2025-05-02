e89d8a8babd9b38aa7671520295070288d108e2c
Jana-script <jana.jscript@gmail.com>
2025-05-02 16:48:05 +0200

    chore RSC: Interactivity with Client Components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,9 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
+
   return (
     <div>
       <h1>Cabins page</h1>
@@ -9,6 +12,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data} />
     </div>
   );
 }
