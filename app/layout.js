20a35cb2c5e6324f4fd469738c9d02607585715e
Jana-script <jana.jscript@gmail.com>
2025-05-08 22:14:42 +0200

    chore Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -18,14 +19,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <main className="flex-1 px-8 py-12">
+          <div className="max-w-7xl mx-auto">{children}</div>
+        </main>
       </body>
     </html>
   );
