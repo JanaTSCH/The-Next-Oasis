8259dcb496072dfa24ffa6978028d965401239de
Jana-script <jana.jscript@gmail.com>
2025-05-26 19:20:20 +0200

    feat sign in
    Building a Custom Sign In Page

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,18 @@
+import { fi } from "date-fns/locale";
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Jonathan
+        Welcome, {firstName}
       </h2>
     </>
   );
